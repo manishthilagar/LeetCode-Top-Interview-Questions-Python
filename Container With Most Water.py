f79f41c7741d11769a2a8204ab96cc285aecc41a
manish thilagar <48599986+manishthilagar@users.noreply.github.com>
2021-01-05 21:29:16 +0530

    Create  Container With Most Water.py

---
+++ Container With Most Water.py
@@ -0,0 +1,19 @@
+    water = 0
+    head = 0
+    tail = len(height) - 1
+
+    for cnt in range(len(height)):
+        
+        width = abs(head - tail)
+        
+        if height[head] < height[tail]:   
+            res = width * height[head]
+            head += 1
+        else:
+            res = width * height[tail]
+            tail -= 1
+
+        if res > water:
+            water = res
+
+    return water
